0edeab44d56ce20288056ffb0864985e1ca43b4f
CarlosMartin2005 <cpenap@unah.hn>
2024-02-10 18:22:55 -0600

    Ejercicios impares

--- Ejercicio_3.dart
+++ Ejercicio_3.dart
@@ -1,3 +1,11 @@
-// Crea un programa el cual imprima una lista con los primeros 15 números de
-// la sucesión de Fibonacci.
+// 3. Crea un programa el cual imprima una lista con los primeros 15 números de la sucesión de Fibonacci.
+void main() {
+  List<int> sequenciafib = [0, 1];
 
+  for (int i = 1; i <= 13; i++) {
+    sequenciafib.add(sequenciafib[i] + sequenciafib[i - 1]);
+  }
+
+  print('Los primeros 15 números de la sucesión de Fibonacci son:');
+  print(sequenciafib);
+}
--- Ejercicio_9.dart
+++ Ejercicio_9.dart
@@ -1,3 +1,16 @@
-// Crea una función que tome una lista de números y devuelva la suma de
-// todos sus elementos. (debe usar un argumento con nombre).
+/*9. Crea una función que tome una lista de números y devuelva la suma de 
+todos sus elementos. (debe usar un argumento con nombre). */
 
+int sumaLista(List<int> elementos) {
+  int suma = 0;
+  for (int elemento in elementos) {
+    suma += elemento;
+  }
+  return suma;
+}
+
+void main() {
+  List<int> lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 23];
+  int resultado = sumaLista(lista);
+  print('La suma de los elementos de la lista es: $resultado');
+}
